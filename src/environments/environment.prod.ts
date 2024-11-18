f95552ef0d491a45aced8076bb11c30bb9294575
Monica Jaña <monicajana1804@gmail.com>
2024-11-18 07:46:34 -0500

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDQBcguYwkJZKL5aSIeX93NHkf8UbOzLN8",
+    authDomain: "proyectom-21417.firebaseapp.com",
+    projectId: "proyectom-21417",
+    storageBucket: "proyectom-21417.firebasestorage.app",
+    messagingSenderId: "123108399587",
+    appId: "1:123108399587:web:115a477852cd4d7d1428d6",
+    measurementId: "G-2DT69V8DHK"
+  }
 };
